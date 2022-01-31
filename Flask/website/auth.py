60a6783796f68aaa9cb334bf4a91c3bbd2db1c85
FannyS <fanny.scheltgen@gmail.com>
2022-01-31 16:09:34 +0100

    Ajout de la méthode POST pour récupérer les données du formulaire pour la prédiction. Ajout des fichiers style.css(actuellement vide) et predict.html

--- Flask/website/auth.py
+++ Flask/website/auth.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/')
+@auth.route('/', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
--- Flask/website/views.py
+++ Flask/website/views.py
@@ -1,7 +1,24 @@
-from flask import Blueprint, render_template
+
+from flask import Blueprint, render_template, request
 
 views = Blueprint('views', __name__)
 
-@views.route('/home')
+@views.route('/home', methods=['GET', 'POST'])
 def home():
-    return render_template("home.html", user = 'Fanny')
+    return render_template("home.html", user = 'ok')
+
+@views.route('/predict', methods=['GET', 'POST'])
+def predict():
+    name = request.form['name']
+    siret = request.form['siret']
+    adresse = request.form['adresse']
+    code_postal = request.form['code_postal']
+    ville = request.form['ville']
+    type_activite = request.form['type_activite']
+    return render_template("predict.html", user = 'ok',
+                                        name=name, 
+                                        siret=siret, 
+                                        adresse=adresse, 
+                                        code_postal=code_postal,
+                                        ville=ville,
+                                        type_activite=type_activite)
